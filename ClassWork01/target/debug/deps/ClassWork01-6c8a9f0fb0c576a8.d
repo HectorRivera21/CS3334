9d69805b9f6091ccd5813abef26061b2725aaa14
Hector <redmagix2000@gmail.com>
2024-02-07 15:08:30 -0600

    test probs added

---
+++ ClassWork01/target/debug/deps/ClassWork01-6c8a9f0fb0c576a8.d
@@ -0,0 +1,5 @@
+C:\Users\hecto\Desktop\coding\CS3334\ClassWork01\target\debug\deps\ClassWork01-6c8a9f0fb0c576a8.exe: src/main.rs
+
+C:\Users\hecto\Desktop\coding\CS3334\ClassWork01\target\debug\deps\ClassWork01-6c8a9f0fb0c576a8.d: src/main.rs
+
+src/main.rs:
